043bc4a141b864cc2255c8467bb2eb2cc5ac9bc3
John-Joseph Gray <jjag3@srcf.net>
2020-10-25 14:45:22 +0000

    fix donors page scaling on mobile

--- donors.inc.php
+++ donors.inc.php
@@ -9,7 +9,8 @@ do {
 		echo mysqli_error($conn);
 		break;
 	}
-	echo '<table class="donors table table-sm">' . "\n";
+	echo '<div class="table-responsive-sm">' . "\n";
+	echo '<table class="donors table">' . "\n";
 	echo '<thead><tr><th>Amount</th><th>Donor</th><th>Date</th></tr></thead><tbody>' . "\n";
 	while ($data = mysqli_fetch_assoc($result)) {
 		echo '<tr><td>&pound;' . htmlspecialchars($data['amount']) . '</td><td>';
@@ -24,5 +25,6 @@ do {
 		echo '</td><td>' . htmlspecialchars($data['date']) . '</td></tr>' . "\n";
 	}
 	echo '</tbody></table>' . "\n";
+	echo '</div>' . "\n";
 } while(0);
 ?>
